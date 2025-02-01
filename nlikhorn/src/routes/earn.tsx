d352706ee7fb6f471e1f01e48295141c73d8cbc7
noegrx <giroux.noe@gmail.com>
2025-02-01 17:25:22 +0100

    feat: add skeleton for earn page

--- nlikhorn/src/routes/earn.tsx
+++ nlikhorn/src/routes/earn.tsx
@@ -1,5 +1,6 @@
+import { Skeleton } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { HeadingContext } from "./__root";
 
 export const Route = createFileRoute("/earn")({
@@ -7,13 +8,18 @@ export const Route = createFileRoute("/earn")({
 });
 
 const KilnWidget = ({ slug }: { slug: string }) => {
+	const [isLoading, setIsLoading] = useState(true);
+
 	return (
-		<iframe
-			src={`https://${slug}.widget${import.meta.env.DEV && ".testnet"}.kiln.fi`}
-			title="Nlik Widget"
-			allow="clipboard-write"
-			style={{ width: "100%", height: "100%", borderRadius: "1rem" }}
-		/>
+		<Skeleton loading={isLoading} height="full" borderRadius="1rem">
+			<iframe
+				src={`https://${slug}.widget${import.meta.env.DEV && ".testnet"}.kiln.fi`}
+				title="Nlik Widget"
+				allow="clipboard-write"
+				style={{ width: "100%", height: "100%", borderRadius: "1rem" }}
+				onLoad={() => setIsLoading(false)}
+			/>
+		</Skeleton>
 	);
 };
 
