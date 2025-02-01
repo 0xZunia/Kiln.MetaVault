fb218141d1370535247aacf05884b575eddd8c0e
noegrx <giroux.noe@gmail.com>
2025-02-02 00:54:18 +0100

    chore: add addressList

--- nlikhorn/src/utils/addressConfig.ts
+++ nlikhorn/src/utils/addressConfig.ts
@@ -1,4 +1,13 @@
+type ContractAddress = `0x${string}`;
+
 export const addressConfig = {
 	FACTORY_ADDRESS:
-		"0x9f2aFA649C0b23661fB51D12ba6fFc552E675507" as `0x${string}`,
+		"0x9f2aFA649C0b23661fB51D12ba6fFc552E675507" as ContractAddress,
+	VAULT_ADDRESS: [
+		"0xdea01fc5289af2c440ca65582e3c44767c0fcf08" as ContractAddress,
+		"0xf3a9a790f84b2e0301069be589fc976cf3eb5661" as ContractAddress,
+		"0x9b80443f910832a6eed6cef5b95bd9d1dae424b5" as ContractAddress,
+		"0x682cfc8a3d956fba2c40791ec8d5a49e13baafbd" as ContractAddress,
+		"0x85fbdc49b2e7b9e07468733873c8f199fc44259f" as ContractAddress,
+	],
 };
