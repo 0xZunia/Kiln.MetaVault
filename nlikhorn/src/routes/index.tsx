23f3bb0a8a24a92f4fc256ee095347d21774d7f7
noegrx <giroux.noe@gmail.com>
2025-02-01 20:43:50 +0100

    factor: ABI factory and wagmi

--- nlikhorn/src/routes/index.tsx
+++ nlikhorn/src/routes/index.tsx
@@ -1,16 +1,19 @@
-import { Box, VStack } from "@chakra-ui/react";
+import { config } from "@/utils/config";
+import { factoryABI } from "@/utils/factoryABI";
+import { Box, Button, VStack } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useContext } from "react";
-import { useAccount, useEnsName } from "wagmi";
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import { HeadingContext } from "./__root";
 
 export const Route = createFileRoute("/")({
 	component: Index,
 });
 
+const FACTORY_ADDRESS = "0x9f2aFA649C0b23661fB51D12ba6fFc552E675507";
+
 function Index() {
 	const { setHeading } = useContext(HeadingContext);
-
 	setHeading("Index");
 
 	return (
--- nlikhorn/src/wagmi.ts
+++ nlikhorn/src/wagmi.ts
@@ -1,6 +1,12 @@
 import { http, createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
 
+declare module "wagmi" {
+	interface Register {
+		config: typeof config;
+	}
+}
+
 export const config = createConfig({
 	chains: [sepolia],
 	transports: {
